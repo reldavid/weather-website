061b0478a9577e45134d2c6132084406061dc19d
ariel david <drel123@gmail.com>
2019-09-14 17:10:44 +0300

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log('client side javascript file loaded')
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const msg1 = document.querySelector('#msg-1')
+const msg2 = document.querySelector('#msg-2')
+
+msg1.textContent = 'from javaScript'
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    msg1.textContent = 'loading...'
+    msg2.textContent = ''
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            
+            if(data.err) {
+                msg1.textContent = (data.err)
+                console.log(data.err)
+            }else{
+                msg1.textContent = (data.location)
+                msg2.textContent = (data.forcast)
+                console.log(data.location)
+                console.log(data.forcast)
+            }
+        })
+    })
+})
